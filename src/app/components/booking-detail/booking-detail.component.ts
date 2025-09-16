5fdfad16e50c035005ca813a72bbdc7cd8ba7d75
daniel.cornejo <daniel.sanchez@geekbears.com>
2025-09-16 13:22:19 -0600

    chore: bootstrap Angular app, add components and service

---
+++ src/app/components/booking-detail/booking-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-detail',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      booking-detail works!
+    </p>
+  `,
+  styles: ``
+})
+export class BookingDetailComponent {
+
+}
